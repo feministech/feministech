c412ea7114c701be61cc251acd10226ff192cef7
Lissa Ferreira <lissadeveloper@gmail.com>
2022-03-21 17:36:42 -0400

    mudando a descrição da feministech

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -3,19 +3,22 @@
     <h1>Sobre a Feministech</h1>
     <p>
       A Feministech é um
-      <strong>
-        grupo de pessoas que se identificam no feminino e não-bináries
-      </strong>
-      que querem produzir, consumir e compartilhar conteúdo sobre tecnologia, em
+      <strong>grupo de pessoas que se identificam no feminino e não-bináries</strong>
+      que produzem, consumem e compartilham conteúdo sobre tecnologia, em
       um ambiente diverso e inclusivo.
     </p>
     <p>
       O objetivo da Feministech é
-      <strong>
-        aumentar a representatividade de minorias no mundo da tecnologia
-      </strong>
+      <strong>aumentar a representatividade de minorias no mundo da tecnologia</strong>
       , que além de serem minorias na tecnologia, também são minorias socias que
       precisam de mais espaço e alcance em todos os espaços.
+    </p>
+
+    <p>
+      Uma das principais missões da Feministech é
+      <strong>auxiliar e incentivar a produção de conteúdo dessas minorias</strong>, como
+      <i>live coding</i>, vídeos, artigos, palestras,
+      <i>podcasts</i> e eventos.
     </p>
   </section>
 </template>
