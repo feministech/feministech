4577979675b0a11bab628427f5bcdfa70428e2ec
doceazedo <me@doceazedo.com>
2023-05-14 22:48:13 -0300

    Adicionar seção "A Feministech"

--- src/lib/blobs/BlobGradientPink.svelte
+++ src/lib/blobs/BlobGradientPink.svelte
@@ -7,7 +7,7 @@
 	xmlns:xlink="http://www.w3.org/1999/xlink"
 	viewBox="0 0 406.78 491.44"
 >
-	<g style="clip-path: url(#clippath)">
+	<g style="clip-path: url(#gradient-pink-clippath)">
 		<image
 			width="409"
 			height="493"
@@ -15,7 +15,7 @@
 			xlink:href={GrainyGradientImg}
 		/>
 	</g>
-	<clipPath id="clippath">
+	<clipPath id="gradient-pink-clippath">
 		<path
 			class="cls-1"
 			d="M91.14,227.78c86.4-37.6,96-93.67,90-117-17.33-16-33-57.2,43-94,95-46,188,9,141,87-47,78,127,215-16,340-143,125-315-32-336-62-21-30-30-107,78-154Z"
---
+++ src/lib/elements/Title.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	export let pretitle = '';
+</script>
+
+{#if pretitle}
+	<p class="pretitle">{pretitle}</p>
+{/if}
+
+<h1 class="title">
+	<slot />
+</h1>
+
+<style lang="scss">
+	@import '../scss/vars';
+
+	.title,
+	.pretitle {
+		text-align: center;
+	}
+
+	.pretitle {
+		color: $primary;
+		font-size: 14px;
+		font-weight: 400;
+		letter-spacing: 0.15em;
+		text-transform: uppercase;
+		margin-bottom: 0.25rem;
+	}
+
+	.title {
+		font-family: $font-title;
+		font-weight: 700;
+		font-size: 32px;
+		margin-bottom: 2.25rem;
+	}
+</style>
