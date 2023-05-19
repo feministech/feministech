a9d5f5103e599d88ee0c27d69cd96712297ff28d
doceazedo <me@doceazedo.com>
2023-05-19 15:29:26 -0300

    Adicionar seção "Nossos Eventos"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Hero from '$lib/layout/Hero.svelte';
 	import AboutSection from '$lib/parts/AboutSection.svelte';
+	import EventsSection from '$lib/parts/EventsSection.svelte';
 </script>
 
 <Hero />
 <AboutSection />
+<EventsSection />
