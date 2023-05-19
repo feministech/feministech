f21bc06b43dcce5ed0c23acb631bd2171fc12de0
doceazedo <me@doceazedo.com>
2023-05-19 16:21:13 -0300

    Adicionar seção "Projetos"

---
+++ src/lib/elements/Card.svelte
@@ -0,0 +1,14 @@
+<div class="card">
+	<slot />
+</div>
+
+<style lang="scss">
+	@import '../scss/vars';
+
+	.card {
+		background: #fff;
+		border: 1px solid rgba(#000, 0.1);
+		box-shadow: 0 0.25rem 0.5rem rgba(#000, 0.05);
+		border-radius: 1rem;
+	}
+</style>
