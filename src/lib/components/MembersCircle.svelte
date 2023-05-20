f0d2d9219453d2d50e6b1ae8c2049ea01a8c34a2
doceazedo <me@doceazedo.com>
2023-05-19 23:18:25 -0300

    Adicionar seção "Quem participa"

---
+++ src/lib/components/MembersCircle.svelte
@@ -0,0 +1,131 @@
+<script lang="ts">
+	import FeministechLogo from '$lib/brand/FeministechLogo.svelte';
+
+	const calculateRingPositions = (ringSize: number, itemCount: number) => {
+		const positions: [number, number][] = [];
+
+		if (itemCount <= 0) return positions;
+
+		const angleStep = (2 * Math.PI) / itemCount;
+		const radius = ringSize / 2;
+
+		for (let i = 0; i < itemCount; i++) {
+			const angle = i * angleStep;
+			const x = radius * Math.cos(angle);
+			const y = radius * Math.sin(angle);
+			positions.push([x, y]);
+		}
+
+		return positions;
+	};
+
+	const rings = [calculateRingPositions(450, 8), calculateRingPositions(780, 16)];
+</script>
+
+<div class="members-circle">
+	{#each rings as circles, i}
+		<div class="members" class:inner={i == 0} class:outer={i == 1}>
+			{#each circles as [x, y]}
+				<a
+					class="member"
+					href="/"
+					target="_blank"
+					rel="noopener noreferrer"
+					style="--x:{x}px;--y:{y}px"
+				>
+					<figure
+						style="background-image:url(https://avatars.dicebear.com/api/adventurer-neutral/{i}{x}{y}.png)"
+					/>
+				</a>
+			{/each}
+		</div>
+	{/each}
+
+	<div class="logo">
+		<FeministechLogo />
+	</div>
+</div>
+
+<style lang="scss">
+	.members-circle {
+		position: relative;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 57rem;
+		padding: 0 4rem;
+	}
+
+	.logo {
+		position: absolute;
+		width: 14rem;
+		height: 14rem;
+	}
+
+	.members {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		transform-origin: center;
+		animation: rotate infinite linear;
+
+		figure {
+			animation: rotate infinite linear reverse;
+		}
+
+		.member {
+			position: absolute;
+			border-radius: 50%;
+			overflow: hidden;
+			transform: translate(var(--x), var(--y));
+			transition: all 0.2s ease;
+
+			&:hover {
+				transform: translate(var(--x), var(--y)) scale(1.1);
+			}
+
+			figure {
+				width: 100%;
+				height: 100%;
+				background-position: center;
+				background-repeat: no-repeat;
+				background-size: cover;
+			}
+		}
+	}
+
+	.inner .member {
+		width: 9rem;
+		height: 9rem;
+	}
+
+	.outer .member {
+		width: 7.5rem;
+		height: 7.5rem;
+	}
+
+	.inner,
+	.inner figure {
+		animation-duration: 120s;
+	}
+
+	.outer,
+	.outer figure {
+		animation-duration: 80s;
+		animation-direction: reverse;
+	}
+
+	.outer figure {
+		animation-direction: normal;
+	}
+
+	@keyframes rotate {
+		from {
+			transform: rotate(0deg);
+		}
+
+		to {
+			transform: rotate(360deg);
+		}
+	}
+</style>
