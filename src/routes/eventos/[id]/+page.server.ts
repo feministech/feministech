079d3d952886bf697bec729f8bb909250d7945e3
doceazedo <me@doceazedo.com>
2023-06-13 22:33:26 -0300

    Tipar eventos e mostrar aviso para eventos sem dados

--- src/routes/eventos/[id]/+page.server.ts
+++ src/routes/eventos/[id]/+page.server.ts
@@ -1,8 +1,23 @@
 import events from '$lib/data/events.json';
 import { error } from '@sveltejs/kit';
 
+type Event = {
+	id: string;
+	title: string;
+	description: string;
+	banner: string;
+	date: string;
+	time?: string;
+	schedule?: {
+		title: string;
+		date?: string;
+		time: string;
+		hosts: string[];
+	}[];
+};
+
 export const load = async ({ params }) => {
-	const event = events.find((event) => event.id === params.id);
+	const event = events.find((event) => event.id === params.id) as Event;
 	if (!event) throw error(404);
 	return { event };
 };
